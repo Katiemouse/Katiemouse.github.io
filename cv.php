354547e2fddf562db9aff1325999fd7a4d196d33
Katiemouse <Katiemouse.Murray@gmail.com>
2016-04-06 21:07:17 +1200

    Cv page

---
+++ cv.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Hello!</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700|Covered+By+Your+Grace" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link href="hello.css" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+    <header>
+      <p>Hello</p>
+      <p>My name is Kate!</p>
+    </header>
+	<nav>
+		<a href="index.html">About Me</a>
+		<a href="cv.php">My CV</a>
+	</nav>
+    <main>
+		<h1>Cirriculum Vitae</h1>
+		
+		<h2>Relevant Work Experience</h2>
+		<ul>
+			<li><h3>Massey University Engineering Camp</h3><br>
+			In 2014 I spent five days at Massey University in Palmerston North, where I, along with 50 other students from the North Island, ran through a first year project with sessions covering different aspects of Engineering and problem solving.
+			</li>
+			<li><h3>Catalyst Open Source Academy</h3><br>
+			This course was held over two weeks in January 205, where I, along with 22 other student got the opportunity to learn about a range of topics relevant to IT including UX, Java, MySQL, Testing, and Accessibility.
+			</li>
+			<li><h3>Four Days work experience for Drupal</h3><br>
+			As part of the Catalyst Open Source Academy, I got the oppurtunity to work with a company for four days. I chose Drupal so that I could utilise my skills in PHP. In these four days I was in a team of four people, and we spent our time working on patches, with three successfully committed between us, and updating the "Beer O'Clock" module from Drupal 7 to 8.
+			</li>
+			<li><h3>Catalyst Arduino Academy</h3><br>
+			This four day course was held in July of 2015 where I, along with seven other students, were picked to learn aboput Arduinos and how they work, as well as how to assemble and code them. On the last day we were given time to work on projects, where my friend and I made together a complex "Pong" Game. <br>
+			<a href="https://github.com/Katiemouse/Pong2015">This</a> was our final code.
+			</li>
+		</ul>
+		<h2>Other Work Experience</h2>
+		<ul>
+			<li><h3>Volunteering at St Margarets Church</h3><br>
+			Over the last eight years I have at various times helped run Sunday School, the Church Cafe, The Girls Brigade Stall at the Church Fair, Girls Brigade and Youth Group.
+			</li>
+			<li><h3>Crew Member at McDonalds</h3><br>
+			I have worked at McDonalds since April 2014 and in April 2016 I became a Create Your Taste Host for my restaurant.
+			</li>
+			<li><h3>Finance and Communications Manager at AwakenSOC</h3><br>
+			I along with 3 other students started up a businees in 2014 for the Lions Young Enterprise Scheme called AwakenSOC. We designed, made and sold 100 left handed ringbinders.
+			</li>
+			<li><h3>Red Badge Security</h3><br>
+			In December 2015 I was hired by Red badge Security to work events like the ACDC Rock or Bust Tour, Wellington Cup Day, The Royal Edinburgh Military Tattoo and the New Zealand vs Pakistan ODI series. For these events I have had a range of jobs, including ticketing, bar service and Coporate jobs, like service and admittance for the Coporate Lounges.
+			</li>
+		</ul>
+		<h2>Skills</h2>
+		<ul>
+			<li><h3>Tecnology</h3><br>
+			My strengths include in PHP, HTML, CSS and Java. I also enjoy working with MySQL, Databases and Wordpress.
+			</li>
+			<li><h3>Miscellaneous</h3><br>
+			I have good communication, leadership skills and work ethic. I work well in teams but can work independantly with ease.
+			</li>
+		</ul>
+		<h2>Education</h2>
+		<ul>
+			<li><h3>St Oran's College</h3><br>
+			From 2009 to 2015 I was a student at St Oran's College. I was a top student with results including:
+				<ul>
+					<li>NCEA Level 1 and 2 endorsed with Excellence.</li>
+					<li>NCEA Level 3 endorsed with Merit.</li>
+					<li>Digital Technologies Level 2 and 3 endorsed with Excellence.</li>
+					<li>Physics Level 2 and 3 endorsed with Merit. </li>
+					<li>Mathematics Level 2 endorsed with Excellence. </li>
+					<li>Calculus Level 3 endorsed with Merit. </li>
+				</ul>
+			</li>
+			<li><h3>Victoria University of Wellington</h3><br>
+			In 2016 I have begun a Bachelor of Engineering (HONS) at Victoria and I plan to major in Software Engineering.
+			</li>
+		</ul>
+		<h2>Hobbies and Activities</h2>
+		<ul>
+			<li><h3>Sports</h3><br>
+				I played touch rugby from 2009 to 2015, and netball from 2005 to 2015. I was a touch coach from 2012 to 2015 and a netball coach and umpire in 2014 and 2015.
+			</li>
+			<li><h3>Cultural</h3><br>
+				<ul>
+					<li><h4>Language</h4> I have been learning French for the last 6 years and plan to take part in the Study Abroad program at Victoria, during which I plan to complete a year of my degree at a French University in order to become fluent.</li>
+					<li><h4>Music</h4> I have played the Clarint for 8 years and the Saxophone for 4. I enjoy playing in my spare time and in groups like Jazz bands and Orchestras.</li>
+					<li><h4>Debating</h4> I have been debating for 3 years, and made the St Oran's College Prem A Debating Team in 2015.</li>
+				</ul>
+			</li>
+		</ul>
+	</main>
+    <footer>
+	  <a href="http://powershop.co.nz"><img src="winky.svg" alt="Powershop"></a>
+      <a href="http://www.summeroftech.co.nz/"><img src="sot-logo.svg" alt="Summer of Tech"></a>
+	  <p>
+		Thanks to the Powershop Crew for helping me with my Github site, and Summer of Tech for the wonderful opportunities.
+	  </p>
+	</footer>
+  </body>
+</html>
